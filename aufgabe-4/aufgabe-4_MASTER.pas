d85daa1765b8a0a428ce689f90bd5e8597b8bc45
mjptree <michael.prantl@hotmail.de>
2019-10-21 10:33:14 +0100

    Create aufgabe-4_MASTER.pas

---
+++ aufgabe-4/aufgabe-4_MASTER.pas
@@ -0,0 +1,26 @@
+program pythagoras(input,output);
+{ Vervollstaendigen Sie das Programm mit der folgende Spezifikation:
+  Eingabe:
+    a, b, c ∈ ℕ
+    
+  Ausgabe:
+    x ∈ {true, false}
+  
+  Nachbedingung:
+    x ist true, genau dann wenn jedes Dreieck mit den Seitenlängen a, 
+    b und c einen rechten Winkel besitzt.
+}
+
+  var 
+  a,b,c : integer;
+  x : boolean;
+
+begin 
+  readln(a);
+  readln(b);
+  readln(c);
+  
+  { Your code goes here... }
+  
+  writeln(x); 
+end.
